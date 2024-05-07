badad4cdf5bf05cbb03aedef0faf452ea306fcef
NiimiSyunya <niimicomputergraphics@gmail.com>
2024-05-07 09:59:05 +0900

    fix: Fixed style

--- app/ui/home/about.tsx
+++ app/ui/home/about.tsx
@@ -6,35 +6,40 @@ import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineDot from '@mui/lab/TimelineDot';
 import { events } from '@/app/data';
+import ScrollAnimation from '@/app/ui/scrollAnimation';
+
 
 export default function About() {
   return (
     <>
-    <div className='p-4'>
-    <h2 className='text-2xl font-semibold mb-4'>主なイベント</h2>
-    <Timeline
-    sx={{
-        [`& .${timelineItemClasses.root}:before`]: {
-          flex: 0,
-          padding: 0,
-        },
-      }}
-      >
-      {events.map((e) => (
-      <TimelineItem key={e.id}>
-        <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>
-            <h1>{e.title}</h1>
-            <p>{e.date}</p>
-            <p>{e.description}</p>
-        </TimelineContent>
-      </TimelineItem>
-    ))}
-    </Timeline>
-    </div>
+      <div className='p-4'>
+        <h2 className='text-2xl font-semibold mb-4'>主なイベント</h2>
+        <Timeline
+          sx={{
+            [`& .${timelineItemClasses.root}:before`]: {
+              flex: 0,
+              padding: 0,
+            },
+          }}
+        >
+          {events.map((e) => (
+            <TimelineItem key={e.id}>
+              <TimelineSeparator>
+                <TimelineDot />
+                <TimelineConnector />
+              </TimelineSeparator>
+              <TimelineContent>
+                <h1>{e.title}</h1>
+                <p>{e.date}</p>
+                <ScrollAnimation >
+                  <p>{e.description}</p>
+                </ScrollAnimation>
+              </TimelineContent>
+            </TimelineItem>
+
+          ))}
+        </Timeline>
+      </div>
     </>
   );
 }
--- app/ui/home/contact.tsx
+++ app/ui/home/contact.tsx
@@ -3,7 +3,7 @@ export default function Contact(){
     return (
         <div className="pt-8 pb-16 flex justify-center items-center flex-col w-4/5 mx-auto my-0">
       <h2 className='text-2xl font-semibold mb-4'>コンタクト</h2>
-      <p className="text-base font-normal mb-4 text-center">もし私に興味のある方はメール，またはX（旧Twitter）のDMまでご連絡ください．
+      <p className="text-base font-normal mb-4 text-center">もし私に興味のある方はメール、またはX（旧Twitter）のDMまでご連絡ください。
       </p>
       <a href="mailto:niimicomputergraphics@gmail.com" className='bg-yellow-400 text-gray-900 hover:bg-[#b3b3b3] px-4 py-2 rounded-md font-semibold transition-all duration-500 ease-in-out cursor-pointer inline-block'>メール</a>
     </div>

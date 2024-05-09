9496236c0ea3b5b0741fb47a7938417666da2549
NiimiSyunya <niimicomputergraphics@gmail.com>
2024-05-09 13:47:05 +0900

    feat: Create projects page

---
+++ app/projects/page.tsx
@@ -0,0 +1,14 @@
+import Hero from "@/app/ui/home/hero";
+import About from "@/app/ui/home/about";
+import Skills from '@/app/ui/home/skills';
+import Projects from '@/app/ui/home/projects';
+import Contact from '@/app/ui/home/contact';
+
+export default function Page(){
+  return (
+    <>
+
+    
+    </>
+  );
+}

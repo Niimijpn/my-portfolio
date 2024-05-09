a016c32108f1abdb9b152f6fb239e162b8021f0a
NiimiSyunya <niimicomputergraphics@gmail.com>
2024-05-09 13:46:21 +0900

    feat: add mui-card component

--- app/ui/home/projects.tsx
+++ app/ui/home/projects.tsx
@@ -4,47 +4,65 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import ScrollAnimation from '@/app/ui/scrollAnimation';
 import AccordionForDescription from "@/app/ui/accordion";
 import Skills from "./skills";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import { CardActionArea } from '@mui/material';
+import Link from "next/link";
 
 
 export default function Projects() {
     return (
         <div className="p-4">
             <h2 className="text-2xl font-semibold mb-4">プロジェクト</h2>
-
             <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
-                {projectData.map((project, index) => (
-                    <ScrollAnimation key={index}>
-                        <div className="border-[1px] border-[#e6e6e6] border-solid p-5 rounded-lg " >
-                            <div className="flex justify-between items-start mb-5">
-                                <FontAwesomeIcon className='text-[35px] text-[#FFCC17]' icon={project.icon} />
-                                <div className="flex gap-x-1">
-                                    <a className="mr-1 cursor-pointer transition-all duration-500 hover:text-yellow-400"
-                                        href={project.gitHubLink}
-                                        aria-label={project.label}
-                                        target="_blank"
-                                        rel="noopener noreferrer">
-                                        <FontAwesomeIcon icon={faGithub} />
-                                    </a>
-                                </div>
-                            </div>
-                            <h3 className="text-[20px] mb-3">{project.title}</h3>
-                            <AccordionForDescription
-                                description={project.description}
-                                skills={Array.isArray(project.skills) ?
-                                    <div style={{ display: 'flex' }}>
-                                        {project.skills.map((skill, index) => (
-                                            <div key={index} style={{ marginRight: '8px' }}>{skill}</div>
-                                        ))}
-                                    </div> :
-                                    []
-                                }
+                {projectData.slice(0, 4).map((project, index) => (
+                    <Card className="bg-[#111]" sx={{ maxWidth: 500 }} key={index}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                alt={project.label}
+                                height="100"
+                                image={project.img}
                             />
+                            <CardContent>
+                                <div className="flex justify-end items-end">
+                                    <div className="flex gap-x-1">
+                                        <a className="mr-1 cursor-pointer transition-all duration-500 hover:text-yellow-400"
+                                            href={project.gitHubLink}
+                                            aria-label={project.label}
+                                            target="_blank"
+                                            rel="noopener noreferrer">
+                                            <FontAwesomeIcon icon={faGithub} />
+                                        </a>
+                                    </div>
+                                </div>
+                                <h3 className="text-[20px] text-[#DADADB]">{project.title}</h3>
+                                <AccordionForDescription
+                                    description={project.description}
+                                    skills={Array.isArray(project.skills) ?
+                                        <div style={{ display: 'flex' }}>
+                                            {project.skills.map((skill, index) => (
+                                                <div key={index} style={{ marginRight: '8px' }}>{skill}</div>
+                                            ))}
+                                        </div> :
+                                        []
+                                    }
+                                />
 
-                        </div>
-
-                    </ScrollAnimation>
+                            </CardContent>
+                        </CardActionArea >
+                    </Card>
                 ))}
             </div>
-        </div>
+            <Link
+                href="/projects"
+            ><strong className="my-6 text-yellow-400 flex justify-end items-end">
+                    もっと見る »
+                </strong>
+            </Link>
+        </div >
     );
 }

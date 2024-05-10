64bef41e19c49efb6aa8feaf2cc0b732266476b5
NiimiSyunya <niimicomputergraphics@gmail.com>
2024-05-10 11:18:34 +0900

    feat: add link

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -2,10 +2,20 @@ import Link from "next/link";
 
 export default function Navbar(){
     return (
-        <div className="flex justify-between items-center p-4 bg-[#1D1E20]">
+        <div className="w-2/5 flex justify-between items-center p-4 bg-[#1D1E20]">
             <div className="text-lg font-bold">
                 <Link href='./'>
                     新美のPortfolio
+                </Link>
+            </div>
+            <div className="text-md font-medium">
+                <Link href='/about'>
+                    About
+                </Link>
+            </div>
+            <div className="text-md font-medium">
+                <Link href='/projects'>
+                    Projects
                 </Link>
             </div>
         </div>

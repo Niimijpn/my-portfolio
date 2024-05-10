eab767774544454fed78dff73fea213a0575f99c
NiimiSyunya <niimicomputergraphics@gmail.com>
2024-05-10 11:18:18 +0900

    feat: add scroll-animation

--- app/ui/home/projects.tsx
+++ app/ui/home/projects.tsx
@@ -1,34 +1,34 @@
 import { projectData } from "@/app/data";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import ScrollAnimation from '@/app/ui/scrollAnimation';
 import AccordionForDescription from "@/app/ui/accordion";
-import Skills from "./skills";
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import { CardActionArea } from '@mui/material';
 import Link from "next/link";
+import ScrollAnimation from '@/app/ui/scrollAnimation';
 
 
-export default function Projects() {
+export default async function Projects() {
     return (
         <div className="p-4">
             <h2 className="text-2xl font-semibold mb-4">プロジェクト</h2>
             <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
                 {projectData.slice(0, 4).map((project, index) => (
-                    <Card className="bg-[#111]" sx={{ maxWidth: 500 }} key={index}>
+                    <ScrollAnimation key={index}>
+                    <Card className="bg-[#111]" sx={{ maxWidth: 500 }} >
                         <CardActionArea>
-                            <CardMedia
+                            <CardMedia className="transform  hover:scale-110 duration-500 ease-in-out"
                                 component="img"
                                 alt={project.label}
                                 height="100"
                                 image={project.img}
                             />
                             <CardContent>
-                                <div className="flex justify-end items-end">
+                                <div className="flex justify-between items-center mt-4">
+
+                                    <h3 className="text-[20px] text-[#DADADB]">{project.title}</h3>
                                     <div className="flex gap-x-1">
                                         <a className="mr-1 cursor-pointer transition-all duration-500 hover:text-yellow-400"
                                             href={project.gitHubLink}
@@ -39,7 +39,7 @@ export default function Projects() {
                                         </a>
                                     </div>
                                 </div>
-                                <h3 className="text-[20px] text-[#DADADB]">{project.title}</h3>
+
                                 <AccordionForDescription
                                     description={project.description}
                                     skills={Array.isArray(project.skills) ?
@@ -55,6 +55,7 @@ export default function Projects() {
                             </CardContent>
                         </CardActionArea >
                     </Card>
+                    </ScrollAnimation>
                 ))}
             </div>
             <Link

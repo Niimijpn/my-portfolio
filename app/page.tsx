7f69d0083ee9b22a7d96f6dda84d32718447e3b9
NiimiSyunya <niimicomputergraphics@gmail.com>
2024-05-10 11:17:42 +0900

    remove about component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Hero from "@/app/ui/home/hero";
-import About from "@/app/ui/home/about";
+import About from "@/app/about/about";
 import Skills from '@/app/ui/home/skills';
 import Projects from '@/app/ui/home/projects';
 import Contact from '@/app/ui/home/contact';
@@ -10,7 +10,6 @@ export default function Page(){
     <Hero/>
     <Skills/>
     <Projects/>
-    <About/>
     <Contact/>
 
 
